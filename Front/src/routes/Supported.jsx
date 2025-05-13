4aa348f8754beba26183f7995bb1897157c832c0
nffdev <nightfrost2discord@outlook.fr>
2025-05-13 18:58:54 +0200

    feat(app): updated logo color

--- Front/src/routes/Supported.jsx
+++ Front/src/routes/Supported.jsx
@@ -166,7 +166,7 @@ export default function SupportedPage() {
                   {
                     name: "QuasarRAT (fully supported)",
                     icon: (
-                      <span className="mr-3 text-red-400 p-2 bg-red-500/10 rounded-lg">
+                      <span className="mr-3 text-green-400 p-2 bg-green-500/10 rounded-lg">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10" /><path d="M8 15h8M9 9h.01M15 9h.01" /></svg>
                       </span>
                     ),
